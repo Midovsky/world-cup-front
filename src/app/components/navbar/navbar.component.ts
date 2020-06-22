87d7e061b8ab8561e4d78b8df7ad53357cca1109
Skander <skanders@live.fr>
2020-06-22 20:00:08 +0100

    add logout

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/utils/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+ 
+
+  constructor(private router : Router) {
+
+  }
 
   ngOnInit(): void {
+  
   }
+
+  logout(): void {
+    localStorage.clear();
+    this.router.navigate(['home']);
+
+  }
+  
+  isAuth (): boolean { 
+    return !!localStorage.getItem('token');
+  }
+
 
 }
