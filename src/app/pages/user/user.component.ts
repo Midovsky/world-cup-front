e97c6aa8f5cdec12b84f7c207c15c32a8475f982
Skander <skanders@live.fr>
2020-06-25 23:40:23 +0100

    add user management service

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from 'src/app/models/user.model';
+import { Router } from '@angular/router';
+import {UserService} from "../../utils/services/user.service";
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -7,9 +9,92 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  
+  rule : string = "" ; 
+  firstName = "" ;
+  lastName = ""; 
+  email ="";
+  password= "";
 
-  ngOnInit(): void {
+  message = ""; 
+
+  disabled = false ;
+
+  isAdd = false;
+  display = true; 
+
+  users : User[] ;
+
+
+
+
+  constructor(private router : Router , private userService: UserService) { 
+      
   }
 
+  ngOnInit() {
+    if (localStorage.getItem('ROLE')==='ROLE_CONSULTANT')
+    this.router.navigate(['/home'])
+
+    this.userService.getAllUsers().subscribe( (res )=> {
+      this.users = res;
+    });
+  }
+
+  register() {
+    
+    if(this.firstName == "" || this.lastName == "" ||  this.email == "" || this.password == ""||this.rule==""){
+      this.message = "all fields are required"; 
+      return 1;
+    }
+
+    this.disabled = true;
+
+    const user = new User();
+    user.email = this.email ; 
+    user.firstName = this.firstName;
+    user.lastName = this.lastName; 
+    user.password =  this.password;
+    
+    console.log(user); 
+
+    this.userService.AddUser(user).subscribe((res) => {
+      
+      
+       
+      this.disabled = false ;
+      this.firstName = "" ;
+      this.lastName = ""; 
+      this.email ="";
+      this.password= "";
+      this.users = res;
+      this.isAdd = false;
+      this.display=true; 
+
+
+    });
+
+    
+
+  }
+
+  addClick(){
+     this.isAdd = !this.isAdd; 
+     this.display=!this.display;
+  }
+
+  delete(id){
+    
+   this.userService.DeleteUser(id).subscribe(res => {
+    console.log(res);
+    this.users = res;
+    
+   });
+
+   console.log('delete ' + id);
+  }
+
+  change($events){
+    console.log(this.rule);
+  }
 }
--- src/app/utils/services/user.service.ts
+++ src/app/utils/services/user.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { User } from 'src/app/models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+   
+  baseurl: string;
+  httpOptions = {
+    headers: new HttpHeaders({
+      "Content-Type": "application/json",
+    }),
+  };
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+  AddUser(obj): Observable<any[]> {
+    return this.http.post<any>(
+      this.baseurl + "/auth/register",
+      obj,
+      this.httpOptions
+    );
+  }
+
+
+  getAllUsers(): Observable<User[]> {
+    return this.http.get<User[]>(
+      this.baseurl + "/user/users",
+      this.httpOptions
+    );
+  }
+
+  DeleteUser(id): Observable<User[]> {
+    return this.http.delete<any>(
+      this.baseurl + "/user/delete/" + id,
+      this.httpOptions
+    );
+  }
 }
