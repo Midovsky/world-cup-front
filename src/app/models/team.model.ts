b81bda83ee581edaaeb1992814921acce5e5dd76
Skander <skanders@live.fr>
2020-06-26 02:00:26 +0100

    add team model flag

--- src/app/models/team.model.ts
+++ src/app/models/team.model.ts
@@ -6,14 +6,16 @@ export class Team {
     public wins: number;
     public loses: number;
     public draws: number;
+    flag: string;
 
   
-    constructor(name: string, wins: number, loses: number, draws: number) {
+    constructor(name: string, wins: number, loses: number, draws: number, flag: string) {
       this.name = name;
 
       this.loses = loses;
       this.wins = wins;
       this.draws = draws;
+      this.flag = flag;
     }
   }
   
